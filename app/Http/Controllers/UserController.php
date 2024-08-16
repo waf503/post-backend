3f4718b2fd95f49e676d0c05781327e50e3e7fcd
Wilmer Flores <wflores@cygnusinv.com>
2024-08-16 09:20:00 -0600

    Events

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,22 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\UserCreated;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index():JsonResponse
     {
-        $users = User::all();
-        return response()->json($users);
+        event(new UserCreated(User::factory()->make()));
+       // $users = User::query()->get();
+        return response()->json([
+            "message" => "hello"
+        ]);
     }
     public function show(User $user):JsonResponse{
         return response()->json($user);
     }
     public function store(Request $request):JsonResponse{
-        return response()->json(["message"=>"ok"]);
+        return DB::transaction(function () use ($request){
+           $created = User::query()->create([
+              'name' => $request->input('name'),
+              'email'=>$request->input('email')
+           ]);
+           event(new UserCreated($created));
+           return $created;
+        });
     }
     public function update(Request $request, User $user):JsonResponse{
         return response()->json(["message"=>"update"]);
